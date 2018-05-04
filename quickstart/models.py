50db8f1124b55b9ea9bf8f81b17087635d3b6992
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-04 17:27:15 +0545

    Personal model addeed and migrated

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Personal(models.Model):
+    Name = models.CharField(max_length=255)
+    Address = models.TextField()
+    PhoneNo = models.IntegerField()
+    
