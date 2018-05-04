cd94ba11b33ebb205f69cbd7b2d19a724cc965aa
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-04 17:39:38 +0545

    Added Route for Personal as Info

--- DRFapp/urls.py
+++ DRFapp/urls.py
@@ -22,6 +22,7 @@ from quickstart import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'info', views.PersonalViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
