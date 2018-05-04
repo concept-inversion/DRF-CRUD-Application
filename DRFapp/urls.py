53a25773ef21e95d4e211cd70541db203326034b
concept-inversion <santosh.pandey2222@gmail.com>
2018-05-04 17:35:50 +0545

    URL for group view fixed

--- DRFapp/urls.py
+++ DRFapp/urls.py
@@ -21,7 +21,7 @@ from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
